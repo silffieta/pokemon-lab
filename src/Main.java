1e4df4285d6c876b1084971c1e464f6a0c4ee366
silffieta <evgeny0624@gmail.com>
2024-11-11 15:20:49 +0300

    few minor details

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,17 @@ import ru.ifmo.se.pokemon.*;
 public class Main {
     public static void main(String[] args){
         Battle b = new Battle();
-        Pokemon p1 = new Vulpix("Ninetales", 50);
+        Pokemon p1 = new Vulpix("Vulpix", 50);
         Pokemon p2 = new Bounsweet("Bounsweet", 50);
+        Pokemon p3 = new Ninetales("Ninetales", 50);
+        Pokemon p4 = new Drampa("Drampa", 50);
+
         b.addAlly(p1);
+        b.addAlly(p3);
+
         b.addFoe(p2);
+        b.addFoe(p4);
+
         b.go();
     }
 }
